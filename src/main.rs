ab5641d5e0ed6a4849b79858d2984f1e58a6fe40
Yudhish <tectrixtech2@gmail.com>
2021-03-29 23:19:08 +0800

    Update main.rs
    Edited the code to include better documentation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main(){
-    println!("Hello, World");
+    println!("Hello, World!"); //Prints Hello, World!
     let mut line = String::new();
-    println!("Enter your name :");
+    println!("Enter your name :"); //Reads STDIN from console
     let _ = std::io::stdin().read_line(&mut line).unwrap();
-    println!("Hello , {}", line);
+    println!("Hello , {}", line); //Prints STDOUT stream
  }
